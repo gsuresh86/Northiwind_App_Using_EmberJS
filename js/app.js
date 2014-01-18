bd38209603490b721549cf8c453ae3f04c4d6348
Suresh G <gsuresh86@gmail.com>
2014-01-18 11:49:13 +0530

    Updated the app

--- js/app.js
+++ js/app.js
@@ -1,334 +1,186 @@
 'use strict';
-//Define an angular module for our app
-var NorthwindApp = angular.module('NorthwindApp', ['ngRoute', 'ngResource']);
+//Define an Ember App
+var NorthwindApp = Ember.Application.create({
+    LOG_TRANSITIONS: true
+});
 
 /* Router */
-NorthwindApp.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/employees', {
-                templateUrl: 'partials/employee-list.html',
-                controller: 'EmployeeController'
-            }).
-            when('/customers', {
-                templateUrl: 'partials/customer-list.html',
-                controller: 'CustomerController'
-            }).
-            when('/products', {
-                templateUrl: 'partials/product-list.html',
-                controller: 'ProductController'
-            }).
-            when('/orders', {
-                templateUrl: 'partials/order-list.html',
-                controller: 'OrderController'
-            }).
-            when('/suppliers', {
-                templateUrl: 'partials/supplier-list.html',
-                controller: 'SupplierController'
-            }).
-            otherwise({
-                redirectTo: '/customers'
-            });
+NorthwindApp.Router.map(function(){
+    this.resource("employees", function(){
+        this.resource("employee", {path: ":EmployeeID"})
+    });
+    this.resource('customers');
+    this.resource('products');
+    this.resource('orders');
+    this.resource('suppliers');
+});
+
+NorthwindApp.IndexRoute = Ember.Route.extend({
+    redirect: function(){
+        this.transitionTo("employees");
     }
-]);
+})
 
-
-/* Controller */
-NorthwindApp.controller('NavigationController', ['$scope',
-    function($scope){
-        $scope.navs = [
-            {id: 1, navID: 'employees', caption: 'Employees'},
-            {id: 2, navID: 'customers', caption: 'Customers'},
-            {id: 3, navID: 'products', caption: 'Products'},
-            {id: 4, navID: 'orders', caption: 'Orders'},
-            {id: 5, navID: 'suppliers', caption: 'Suppliers'}
+/* Application Route */
+NorthwindApp.ApplicationRoute = Ember.Route.extend({
+        model: function(){
+        return [
+            {id: 1, routeID: 'employees', caption: 'Employees'},
+            {id: 2, routeID: 'customers', caption: 'Customers'},
+            {id: 3, routeID: 'products', caption: 'Products'},
+            {id: 4, routeID: 'orders', caption: 'Orders'},
+            {id: 5, routeID: 'suppliers', caption: 'Suppliers'}
         ];
     }
-]);
+});
 
-NorthwindApp.controller('EmployeeController', ['$scope', 'NorthwindService',
-    function($scope, NorthwindService){
-        $scope.$parent.selected = 1;
+/* Employee Route and Model */
+NorthwindApp.Employee = Ember.Object.extend({
+    ImageUrl: function(){
+        return 'img/profile/'+this.get("EmployeeID")+'.jpg';
+    }.property('EmployeeID')
+});
 
-        NorthwindService.getData('data/employees.json')
-            .success(function(response){
-                $scope.employees = response;
-                $scope.selected = $scope.employeeInfo = $scope.employees[0];
-                $scope.showEmployee = true;
-            })
-            .error(function(response){ /*todo error case */ });
-
-        $scope.handleClick = function($event){
-            if ($event.stopPropagation) $event.stopPropagation();
-            if ($event.preventDefault) $event.preventDefault();
-            $scope.selected = $scope.employeeInfo = this.employee;
-            $scope.showEmployee = true;
-        };
-
-        $scope.isSelected = function(employee) {
-            return $scope.selected === employee;
-        };
-
-        $scope.hideEmpInfo = function($event){
-            $scope.showEmployee = false;
-            $scope.selected = "";
-            if ($event.stopPropagation) $event.stopPropagation();
-            if ($event.preventDefault) $event.preventDefault();
-        };
-    }
-]);
-
-NorthwindApp.controller('CustomerController', ['$scope', 'NorthwindService',
-    function($scope, NorthwindService){
-
-        $scope.$parent.selected = 2;
-        NorthwindService.trigger('loadMap');
-
-        NorthwindService.getData('data/customers.json')
-            .success(function(response){
-                $scope.customers = response;
-                var customer = $scope.customers[0];
-                $scope.selected = customer;
-                $scope.showLocation(customer);
-            })
-            .error(function(response){ /*todo error case */ });
-
-        NorthwindService.getData('data/orders.json').success(function(response){ $scope.orders = response; });
-
-        $scope.handleRowClick = function($event){
-            $scope.selected = this.customer;
-            $scope.selCustomer = this.customer.CustomerID;
-            $scope.showLocation(this.customer);
-        };
-
-        $scope.isSelected = function(customer) {
-            return $scope.selected === customer;
-        };
-
-        $scope.showLocation = function(customer){
-            var address = customer.Address + "," + customer.City + "," + customer.Country;
-            NorthwindService.trigger('locateByAddress', address);
-        }
-    }
-]);
-
-NorthwindApp.controller('OrderController', ['$scope', 'NorthwindService',
-    function($scope, NorthwindService){
-        $scope.$parent.selected = 4;
-        $scope.showProduct = false;
-        $scope.currentPage = 0;
-        $scope.pageSize = 10;
-        $scope.orders = [];
-        $scope.selPage = 1;
-        NorthwindService.getData('data/orders.json')
-            .success(function(response){
-                $scope.orders = response;
-                $scope.noOfPages = Math.round($scope.orders.length/$scope.pageSize)
-            })
-            .error(function(response){ /*todo error case */ });
-
-        $scope.handleRowClick = function(){
-            $scope.selected = this.order;
-            $scope.showOrder = true;
-            $scope.orderInfo = this.order;
-        };
-
-        $scope.test = function(){
-            alert($scope.selPage);
-        }
-
-
-        $scope.isSelected = function(order) {
-            return $scope.selected === order;
-        }
-}]);
-
-NorthwindApp.controller('ProductController', ['$scope', '$rootScope', 'NorthwindService',
-    function($scope, $rootScope, NorthwindService){
-        $scope.$parent.selected = 3;
-        $scope.showProduct = false;
-        $scope.currentPage = 0;
-        $scope.pageSize = 15;
-        $scope.products = [];
-        NorthwindService.getData('data/products.json')
-            .success(function(response){
-                $scope.initialize(response);
-            })
-            .error(function(response){ /*todo error case */ });
-
-        $scope.initialize = function(products){
-            angular.forEach(products, function(product){
-                product.prodImg = 'img/product/' + product.ProductID + '.png';
-                $scope.products.push(product);
+NorthwindApp.EmployeesRoute = Ember.Route.extend({
+    model: function(){
+        return $.getJSON('data/employees.json').then(function(response){
+            var employees = [];
+            $.each(response, function(index, employee){
+                employees.push(NorthwindApp.Employee.create(employee))
             });
-            $scope.noOfPages =  $scope.products.length/$scope.pageSize;
-        };
-
-        $scope.handleRowClick = function(){
-            $scope.showProduct = true;
-            $scope.$parent.showOverlay = true;
-            $scope.productInfo = this.product;
-            angular.element(".prod-detail input")[0].focus();
-        };
-
-        $scope.handleKeyDown = function(){
-            if (event.keyCode == 27) {
-                $scope.showProduct = false;
-                $scope.$parent.showOverlay = false;
-            } else {
-                return;
-            }
-        };
-
-        $scope.isSelected = function(product) {
-            return $scope.selected === product;
-        }
+            return employees;
+        });
     }
-]);
-
-NorthwindApp.controller('SupplierController', ['$scope', 'NorthwindService',
-    function($scope, NorthwindService){
-        $scope.$parent.selected = 5;
-        NorthwindService.getData('data/suppliers.json')
-            .success(function(response){ $scope.suppliers = response; })
-            .error(function(response){ /*todo error case */ });
-    }
-]);
-
-/* Service */
-/*
-NorthwindApp.service('NorthwindService', ['$resource', '$rootScope', '$http', function($resource, $rootScope, $http){
-        this.trigger = function(method) {
-            var args = Array.prototype.slice.call(arguments, 1);
-            args = args.join(',');
-            $rootScope.$broadcast(method, args);
-        }
-
-        this.getData = function(url){
-            return $http.get(url);
-        }
-    }]
-);
-*/
-
-/* Factory */
-NorthwindApp.factory('NorthwindService', ['$rootScope', '$http',
-    function($rootScope, $http){
-        return {
-
-            trigger: function(method) {
-                var args = Array.prototype.slice.call(arguments, 1);
-                args = args.join(',');
-                $rootScope.$broadcast(method, args);
-            },
-
-            getData: function(url){
-                return $http.get(url);
-            }
-        };
-    }]);
-
-/* Directive */
-NorthwindApp.directive('orderDetail', function(){
-    return{
-        restrict: 'A',
-        templateUrl : 'partials/order-detail.html'
-    };
 });
 
-NorthwindApp.directive('productDetail', function(){
-    return{
-        restrict: 'A',
-        templateUrl : 'partials/product-detail.html',
-        link: function(scope, element){
-            scope.close = function(){
-                this.showProduct = false;
-                this.$parent.showOverlay = false;
-            }
-        }
-    };
+/* Product Route and Model */
+NorthwindApp.Product = Ember.Object.extend({
+    ProductImg: function(){
+        return 'img/product/'+this.get("ProductID") + '.png';
+    }.property('ProductID')
 });
 
-NorthwindApp.directive('employeeDetail', function(){
-    return{
-        restrict: 'A',
-        templateUrl : 'partials/employee-detail.html',
-        link: function(scope, element){
-            scope.close = function(){
-                this.showEmployee = false;
-            }
-        }
-    };
-});
+NorthwindApp.ProductsRoute = Ember.Route.extend({
 
-NorthwindApp.directive('ngGoogleMap', function(){
-    return {
-        restrict: 'E',
-        link: function(scope, element){
-            scope.init = function(){
-                if(typeof(google) == "undefined"){
-                    scope.el.html("<p><h1>You are not connected to internet to access the Google Map</h1></p>");
-                    return;
-                }
-                google.maps.visualRefresh = true;
-                var mapOptions = {
-                    zoom: 10,
-                    center: new google.maps.LatLng(1, 1),
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                scope.map = new google.maps.Map(element[0], mapOptions);
-                scope.marker = new google.maps.Marker({ map: scope.map});
-            }
-
-            scope.$on('locateByAddress', function(event, data) {
-                scope.locateByAddress(data);
+    model: function(){
+        var that = this;
+        var products = []
+        return $.getJSON('data/Products.json').then(function(response){
+            $.each(response, function (index, product) {
+                products.push(NorthwindApp.Product.create(product));
             });
+            return products;
+        });
+    }
+});
 
-            scope.$on('loadMap', function(event, data) {
-                scope.init();
-            });
+NorthwindApp.ProductsController = Ember.Controller.extend({
+    currentPage: 0,
+    limit: 15,
+    filteredProducts : function(){
+        return this.get('model').slice(this.currentPage*this.limit, (this.currentPage+1)*this.limit)
+    }.property('currentPage'),
 
-            scope.locateByAddress = function(address){
-                var position = null;
-                var geocoder = new google.maps.Geocoder();
-                geocoder.geocode({ 'address': address }, function(results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        position =  results[0].geometry.location;
-                        scope.render(position);
-                    }
-                });
-            };
-
-            scope.render = function(position){
-                scope.map.setCenter(position);
-                scope.marker.setPosition(position);
-                //this.marker.setTitle(this.model.get("title"));
-                scope.marker.setAnimation("BOUNCE");
-            };
-
-            scope.init();
+    actions: {
+        navRight: function(){
+            var currentPage = this.get('currentPage');
+            this.set('currentPage', currentPage+1);
+        },
+        navLeft: function(){
+            var currentPage = this.get('currentPage');
+            this.set('currentPage', currentPage-1);
         }
     }
 });
 
-NorthwindApp.directive('ngOverlay', function(){
-    return{
-        restrict: 'E',
-        link: function(scope){
-            scope.showOverlay = function(){
-
-            }
-            scope.hideOverlay = function(){
-
-            }
-        }
-    };
+/* Customer Route and Model */
+NorthwindApp.CustomersRoute = Ember.Route.extend({
+    model: function(){
+        return $.getJSON('data/customers.json');
+    }
 });
 
-/* Filter */
-NorthwindApp.filter('startFrom', function() {
-    return function(data, index){
-        index = +index; //parse to int
-        return data.slice(index);
+/* Order Route and Model */
+NorthwindApp.OrdersRoute = Ember.Route.extend({
+    model: function(){
+        var that = this;
+        var orders = [];
+        return $.getJSON('data/Orders.json').then(function(response){
+            $.each(response, function (index, order) {
+                orders.push(Ember.Object.create(order));
+            });
+            return orders;
+        });
+    }
+});
+
+NorthwindApp.OrdersController = Ember.Controller.extend({
+    currentPage: 1,
+    pageSize: 10,
+    selPage: 1,
+    showAllClass: "collapse",
+    showAllCaption: "ShowAll",
+    filteredProducts : function(){
+        this.set('noOfPages', Math.ceil(this.get('model').length/this.pageSize));
+        return this.get('model').slice((this.currentPage-1)*this.pageSize, (this.currentPage)*this.pageSize)
+    }.property('currentPage', 'pageSize'),
+    disableLeft: function(){ return this.currentPage == 1; }.property('currentPage'),
+    disableFirst: function(){
+        return this.currentPage == 1;
+    }.property('currentPage'),
+    disableRight: function(){
+        return this.currentPage == this.noOfPages;
+    }.property('currentPage', 'noOfPages'),
+    disableLast: function(){
+        return this.currentPage == this.noOfPages;
+    }.property('currentPage', 'noOfPages'),
+    disableGoto: function(){
+        return this.pageSize == this.get('model').length;
+    }.property('pageSize'),
+    actions: {
+        navRight: function(){
+            var currentPage = this.get('currentPage');
+            if(currentPage < this.get('noOfPages'))
+                this.set('currentPage', currentPage+1);
+        },
+        navLeft: function(){
+            var currentPage = this.get('currentPage');
+            if(currentPage > 0)
+                this.set('currentPage', currentPage-1);
+        },
+        navFirst: function(){
+            this.set('currentPage', 1);
+        },
+        navLast: function(){
+            this.set('currentPage', this.get('noOfPages'));
+        },
+        showAll: function(){
+            var pageSize= this.get("pageSize");
+            if(pageSize != 10){
+                pageSize = 10;
+                this.set('showAllCaption', 'Collapse');
+                this.set('showAllClass', "collapse")
+            }else {
+                pageSize = this.get('model').length;
+                this.set('showAllClass', "showAll");
+                this.set('showAllCaption', 'ShowAll');
+            }
+            this.set('currentPage',1);
+            this.set('pageSize',pageSize);
+        },
+        gotoPage: function(){
+            this.set('currentPage', this.get("selPage"));
+        }
+    }
+});
+
+NorthwindApp.OrdersView = Ember.View.extend({
+
+});
+
+/* Supplier Route */
+NorthwindApp.SuppliersRoute = Ember.Route.extend({
+    model: function(){
+        return $.getJSON('data/suppliers.json');
     }
 });
